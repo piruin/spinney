3092e94ccb93fe2d51b6a7014b550fe52087d269
Piruin Panichphol <piruin.p@gmail.com>
2017-07-28 16:25:45 +0700

    Refactor

--- spinney/src/main/java/me/piruin/spinney/SpinneyAdapter.java
+++ spinney/src/main/java/me/piruin/spinney/SpinneyAdapter.java
@@ -2,6 +2,7 @@ package me.piruin.spinney;
 
 import android.content.Context;
 import android.support.annotation.LayoutRes;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -84,30 +85,35 @@ public final class SpinneyAdapter<T> extends BaseAdapter implements Filterable {
   }
 
   @Override public Filter getFilter() {
-    return new Filter() {
-      @Override protected FilterResults performFiltering(final CharSequence constraint) {
-        FilterResults results = new FilterResults();
-        if (constraint == null || constraint.length() == 0) {
-          results.values = conditionedItem;
-          results.count = conditionedItem.size();
-        } else {
-          List<T> filteredList = new ArrayList<>();
-          String query = constraint.toString().toLowerCase(Locale.getDefault());
-          for (T item : conditionedItem) {
-            if (presenter.getLabelOf(item, 0).toLowerCase(Locale.getDefault()).contains(query))
-              filteredList.add(item);
-          }
-          results.values = filteredList;
-          results.count = filteredList.size();
-        }
-        return results;
-      }
+    return new FilterByLabel();
+  }
 
-      @Override
-      protected void publishResults(CharSequence constraint, final FilterResults results) {
-        filteredItem = (List<T>) results.values;
-        notifyDataSetChanged();
+  private class FilterByLabel extends Filter {
+
+    private final Locale locale = Locale.getDefault();
+
+    @Override protected FilterResults performFiltering(final CharSequence constraint) {
+      FilterResults results = new FilterResults();
+      if (TextUtils.isEmpty(constraint)) {
+        results.values = conditionedItem;
+        results.count = conditionedItem.size();
+      } else {
+        List<T> filteredList = new ArrayList<>();
+        String query = constraint.toString().toLowerCase(locale);
+        for (T item : conditionedItem) {
+          if (presenter.getLabelOf(item, 0).toLowerCase(locale).contains(query))
+            filteredList.add(item);
+        }
+        results.values = filteredList;
+        results.count = filteredList.size();
       }
-    };
+      return results;
+    }
+
+    @Override
+    protected void publishResults(CharSequence constraint, final FilterResults results) {
+      filteredItem = (List<T>) results.values;
+      notifyDataSetChanged();
+    }
   }
 }
